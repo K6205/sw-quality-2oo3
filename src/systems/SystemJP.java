6439f9eab11bec8220b202746be78264ca92b659
K6205 <39126951+K6205@users.noreply.github.com>
2019-03-20 18:01:46 +0100

    added SystemJP

---
+++ src/systems/SystemJP.java
@@ -0,0 +1,15 @@
+package systems;
+
+import interfaces.ISystem;
+
+public class SystemJP implements ISystem {
+
+    @Override
+    public double calcReliability(double[] failureRates, double t) {
+        double product = 0.0;
+        for(int i = 0; i <= failureRates.length; i++){
+            product = (Math.exp(failureRates[0]*t));
+        }
+        return (1 - product);
+    }
+}
