1eaf766fbf64931ce0089d4671206861ca6dc7f6
K6205 <39126951+K6205@users.noreply.github.com>
2019-03-20 18:09:29 +0100

    added IVoter

---
+++ src/interfaces/IVoter.java
@@ -0,0 +1,14 @@
+package interfaces;
+
+import sun.plugin.JavaRunTime;
+
+public interface IVoter {
+
+    /**
+     *
+     * @param results 3 results from 3 ISystems
+     * @return result based on 2/3 vote
+     * @throws RuntimeException if not 3 elements in result or 2/3 vote fails
+     */
+    double vote(double[] results) throws RuntimeException;
+}
