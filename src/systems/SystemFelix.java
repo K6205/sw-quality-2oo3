e618c4fe383a7b849351dfee891e8f161c4e970b
Felix Neubauer <felix@neuby.de>
2019-03-20 17:52:38 +0100

    added example system

---
+++ src/systems/SystemFelix.java
@@ -0,0 +1,11 @@
+package systems;
+
+import interfaces.ISystem;
+
+public class SystemFelix implements ISystem {
+
+    @Override
+    public double calcReliability(double[] failureRates, double t) {
+        return 0;
+    }
+}
