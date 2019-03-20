544f6dad973254f1a33adc9d1f9cc82a3a50fe43
Felix Neubauer <felix@neuby.de>
2019-03-20 18:31:19 +0100

    updated voter

--- src/interfaces/VoteFailException.java
+++ src/interfaces/VoteFailException.java
@@ -2,6 +2,10 @@ package interfaces;
 
 public class VoteFailException extends Exception{
 
+    public final static int REASON_NO_MATCH = 0;
+    public final static int REASON_NO_MAJORITY = 1;
+
+
     private int reason;
 
     public VoteFailException(String message, int reason){
--- src/voter/MajorityVoter.java
+++ src/voter/MajorityVoter.java
@@ -1,6 +1,7 @@
 package voter;
 
 import interfaces.IVoter;
+import interfaces.VoteFailException;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -10,18 +11,24 @@ public class MajorityVoter implements IVoter {
 
 
     @Override
-    public double vote(double[] results) throws RuntimeException {
+    public double vote(double[] results) throws RuntimeException, VoteFailException {
         Map<Double, Integer> stats = new LinkedHashMap<>();
         for (double result : results) {
             int votes = stats.getOrDefault(result, 0);
             stats.put(result, votes +1);
         }
+        boolean someIdenticalResults = false;
         for (double key : stats.keySet()) {
             int votes = stats.get(key);
+            if (votes >= 2) {
+                someIdenticalResults = true;
+            }
             if (votes > results.length / 2) {
                 return key;
             }
         }
-        throw new RuntimeException("No majority.");
+
+        int errorType = someIdenticalResults ? VoteFailException.REASON_NO_MAJORITY : VoteFailException.REASON_NO_MATCH;
+        throw new VoteFailException("No majority.", errorType);
     }
 }
