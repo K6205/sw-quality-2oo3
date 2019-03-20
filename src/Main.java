57ae0b8a2a69a05c54634030f79bce5262b9b532
K6205 <39126951+K6205@users.noreply.github.com>
2019-03-20 20:41:25 +0100

    General refactoring and bugfixing

--- src/Main.java
+++ src/Main.java
@@ -7,22 +7,44 @@ import interfaces.*;
 import voter.*;
 
 public class Main {
-    public static void main(String[] args) {
-       double[] fR = new double[]{ 0.5, 0.43, 0.18 };
+    public static void main(String[] args){
+        double t = 1;
+
+        String s = javax.swing.JOptionPane.showInputDialog("n:");
+        int n = Integer.parseInt(s);
+
+        ArrayList<Double> failureRates = new ArrayList<>();
+
+        String message;
+        for(int i=0; i<n; i++){
+            message = "Failure rate "+(i+1);
+            s = javax.swing.JOptionPane.showInputDialog(message);
+            failureRates.add(Double.parseDouble(s));
+        }
+
         ISystem daniel = new SystemDaniel();
         ISystem jp = new SystemJP();
         ISystem felix = new SystemFelix();
 
         IVoter m = new MajorityVoter();
-        double t = 1.0;
-        double[] r = {daniel.calcReliability(fR, t), jp.calcReliability(fR,t), felix.calcReliability(fR, t)};
-        double res = -1;
+
+        Double[] fR = new Double[n];
+        failureRates.toArray(fR);
+
+        double[] results = {daniel.calcReliability(fR, t), jp.calcReliability(fR,t), felix.calcReliability(fR, t)};
+
         try {
-            res = m.vote(r);
+            double vote = m.vote(results);
+            System.out.println(vote);
         }catch (VoteFailureException VFD){
-            //do something
+           if (VFD.getReason() == VoteFailureException.REASON_NO_MATCH){
+               System.out.println("Not enough votes.");
+           }else if (VFD.getReason() == VoteFailureException.REASON_NO_MAJORITY){
+               System.out.println("No consensus reached.");
+           }else{
+               System.out.println("Unknown VoteFailureException.");
+           }
         }
-        System.out.println(res);
     }
 
 }
