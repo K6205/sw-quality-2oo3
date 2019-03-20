c02bc491eefe290c39ab8205bdec665ef89a9bb1
K6205 <39126951+K6205@users.noreply.github.com>
2019-03-20 19:06:47 +0100

    fixed some of the mess in Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,28 @@
 import javax.swing.*;
+import java.util.ArrayList;
+
+import exceptions.VoteFailureException;
+import systems.*;
+import interfaces.*;
+import voter.*;
 
 public class Main {
     public static void main(String[] args) {
-       //TODO
+       double[] fR = new double[]{ 0.5, 0.43, 0.18 };
+        ISystem daniel = new SystemDaniel();
+        ISystem jp = new SystemJP();
+        ISystem felix = new SystemFelix();
+
+        IVoter m = new MajorityVoter();
+        double t = 1.0;
+        double[] r = {daniel.calcReliability(fR, t), jp.calcReliability(fR,t), felix.calcReliability(fR, t)};
+        double res = -1;
+        try {
+            res = m.vote(r);
+        }catch (VoteFailureException VFD){
+            //do something
+        }
+        System.out.println(res);
     }
 
 }
